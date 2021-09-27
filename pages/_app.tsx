bc55a1200c7fe0be46a29992bce95cb2adfd8eb7
ansango <anibalsantosgo@gmail.com>
2021-09-27 22:12:01 +0200

    config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
