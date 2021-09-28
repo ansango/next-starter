0c026f4fbec6dbcb67030304f5155df04fb98813
ansango <anibalsantosgo@gmail.com>
2021-09-28 19:44:13 +0200

    translate

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import siteMetadata from "@/config/siteMetadata";
 import styles from "@/styles/components/ui/Footer.module.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <Image src="/static/images/then.png" alt="Vercel Logo" width={72} height={72} />
+      <span>{siteMetadata.author}</span>
+      <span>-</span>
+      <span>{currentYear}©️</span>
     </footer>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import styles from "@/styles/Home.module.css";
 
+import tools from "@/lib/mock/tools";
+
 export async function getStaticProps({ locale }: any) {
   return {
     props: {
@@ -14,22 +16,26 @@ export async function getStaticProps({ locale }: any) {
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
-  console.log(t("home:welcome_msg"));
+  const tTools: any = t("home:tools", { returnObjects: true });
   return (
     <>
       <PageSeo title="Next.js starter" description="A Next.js Starter" />
       <></>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>{t("home:welcome_msg")}</h1>
-        <p className={styles.description}>Configuración del starter</p>
+        <h1 className={styles.title}>{t("home:title")}</h1>
+        <p className={styles.description}>{t("home:description")}</p>
         <ul className={styles.grid}>
-          {tools.map((tool) => (
-            <li key={tool.name} className={styles.card}>
-              <h2>{t(`home:${tool.name}`)}</h2>
-              <p>{t(`home:${tool.description}`)}</p>
-            </li>
-          ))}
+          {tools.map((tool: any) => {
+            return (
+              <li key={tool.name} className={styles.card}>
+                <a href={tool.url}>
+                  <h2>{tool.name}</h2>
+                  <p>{tTools[tool.key]}</p>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </main>
     </>
@@ -37,56 +43,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const tools = [
-  {
-    name: "Next.js",
-    url: "https://nextjs.org/",
-    description: "The React Framework",
-  },
-  {
-    name: "Typescript",
-    url: "https://www.typescriptlang.org/",
-    description: "Typed superset of JavaScript",
-  },
-  {
-    name: "ESLint",
-    url: "https://eslint.org/",
-    description: "Code quality tool",
-  },
-  {
-    name: "Prettier",
-    url: "https://prettier.io/",
-    description: "A code formatter",
-  },
-  {
-    name: "Husky",
-    url: "https://github.com/typicode/husky",
-    description: "Modern native Git hooks",
-  },
-  {
-    name: "Lint-Staged",
-    url: "https://github.com/okonet/lint-staged",
-    description: "Run linters on git staged files",
-  },
-  {
-    name: "next-i18next",
-    url: "https://github.com/isaachinman/next-i18next",
-    description: "Translate your NextJs apps.",
-  },
-  {
-    name: "i18nexus",
-    url: "https://i18nexus.com/",
-    description: "Automate your translations ",
-  },
-  {
-    name: "Sitemap Generator",
-    url: "",
-    description: "Sitemap ready on deploy",
-  },
-  {
-    name: "RSS Generator",
-    url: "",
-    description: "Feed ready on deploy",
-  },
-];
